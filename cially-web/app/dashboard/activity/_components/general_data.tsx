ecd753809f032d1c62d73f321d723bc761f5fe60
skellgreco <tixisb@gmail.com>
2025-05-23 21:30:54 +0300

    Added "not enough data" indicators in message tab charts to avoid client side errors

--- cially-web/app/dashboard/activity/_components/general_data.tsx
+++ cially-web/app/dashboard/activity/_components/general_data.tsx
@@ -3,71 +3,90 @@ import { Card } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function GeneralActivityData({ chartData }) {
-	if (!chartData) {
-		return (
-			<>
-				<Card className="mt-10 grid  auto-rows-auto px-10 sm:min-w-dvh">
-					<div>
-						<div className="text-xl font-semibold">
-							<Activity className="inline mr-2" />
-							General Data
-						</div>
-						<div className="font-sans text-sm mt-1 text-white/60">
-							More insights regarding guild's activity
-						</div>
-					</div>
-					<Skeleton className="w-full h-15" />
-				</Card>
-			</>
-		);
-	}
+  if (!chartData) {
+    return (
+      <>
+        <Card className="mt-10 grid  auto-rows-auto px-10 sm:min-w-dvh">
+          <div>
+            <div className="text-xl font-semibold">
+              <Activity className="inline mr-2" />
+              General Data
+            </div>
+            <div className="font-sans text-sm mt-1 text-white/60">
+              More insights regarding guild's activity
+            </div>
+          </div>
+          <Skeleton className="w-full h-15" />
+        </Card>
+      </>
+    );
+  }
 
-	const ArrayChartData = Array(chartData)[0];
-	console.log(ArrayChartData);
+  try {
+    const ArrayChartData = Array(chartData)[0];
+    console.log(ArrayChartData);
 
-	return (
-		<>
-			<Card className="mt-10 grid  auto-rows-auto px-10 sm:min-w-dvh">
-				<div>
-					<div className="text-xl font-semibold">
-						<Activity className="inline mr-2" />
-						General Data
-					</div>
-					<div className="font-sans text-sm mt-1 text-white/60">
-						More insights regarding guild's activity
-					</div>
-				</div>
-				<div className="grid grid-cols-2 ">
-					<div className="">
-						<div>
-							Total Members:{" "}
-							<p className="inline text-white/80 font-sans">
-								{ArrayChartData[0].total}
-							</p>
-						</div>
-						<div>
-							Online Members:{" "}
-							<p className="inline text-white/80 font-sans">
-								{ArrayChartData[0].online}
-							</p>
-						</div>
-					</div>
-					<div>
-						<div>
-							Idle Members:{" "}
-							<p className="inline text-white/80 font-sans">
-								{ArrayChartData[0].idle}
-							</p>
-						</div>
-						<div>
-							Offline Members:{" "}
-							<p className="inline text-white/80 font-sans">
-								{ArrayChartData[0].offline}
-							</p>
-						</div>
-					</div>
-				</div>
-			</Card>
-		</>
-	);
+    return (
+      <>
+        <Card className="mt-10 grid  auto-rows-auto px-10 sm:min-w-dvh">
+          <div>
+            <div className="text-xl font-semibold">
+              <Activity className="inline mr-2" />
+              General Data
+            </div>
+            <div className="font-sans text-sm mt-1 text-white/60">
+              More insights regarding guild's activity
+            </div>
+          </div>
+          <div className="grid grid-cols-2 ">
+            <div className="">
+              <div>
+                Total Members:{" "}
+                <p className="inline text-white/80 font-sans">
+                  {ArrayChartData[0].total}
+                </p>
+              </div>
+              <div>
+                Online Members:{" "}
+                <p className="inline text-white/80 font-sans">
+                  {ArrayChartData[0].online}
+                </p>
+              </div>
+            </div>
+            <div>
+              <div>
+                Idle Members:{" "}
+                <p className="inline text-white/80 font-sans">
+                  {ArrayChartData[0].idle}
+                </p>
+              </div>
+              <div>
+                Offline Members:{" "}
+                <p className="inline text-white/80 font-sans">
+                  {ArrayChartData[0].offline}
+                </p>
+              </div>
+            </div>
+          </div>
+        </Card>
+      </>
+    );
+  } catch (err) {
+    return (
+      <>
+        <Card className="mt-10 grid  auto-rows-auto px-10 sm:min-w-dvh">
+          <div>
+            <div className="text-xl font-semibold">
+              <Activity className="inline mr-2" />
+              General Data
+            </div>
+            <div className="font-sans text-sm mt-1 text-white/60">
+              More insights regarding guild's activity
+            </div>
+          </div>
+          <div className="text-center">Not enough data</div>
+        </Card>
+      </>
+    );
+  }
 }
--- cially-web/app/dashboard/messages/_components/_message-charts/last_7d.tsx
+++ cially-web/app/dashboard/messages/_components/_message-charts/last_7d.tsx
@@ -2,138 +2,167 @@
 import { TrendingDown, TrendingUp } from "lucide-react";
 import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import {
-	type ChartConfig,
-	ChartContainer,
-	ChartTooltip,
-	ChartTooltipContent,
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
 } from "@/components/ui/chart";
 import { Skeleton } from "@/components/ui/skeleton";
 
 const chartConfig = {
-	amount: {
-		label: "amount",
-		color: "#03d5ff",
-	},
+  amount: {
+    label: "amount",
+    color: "#03d5ff",
+  },
 } satisfies ChartConfig;
 
 export default function Last7d({ chartData }) {
-	if (!chartData) {
-		return (
-			<>
-				<Card>
-					<CardHeader>
-						<CardTitle>Last 7 days</CardTitle>
-						<CardDescription>
-							Showing total messages for the last 7 days
-						</CardDescription>
-					</CardHeader>
-					<CardContent>
-						<Skeleton className="w-[250px] h-[150px] place-self-center rounded-xl" />
-					</CardContent>
-					<CardFooter>
-						<div className="flex w-full items-start gap-2 text-sm">
-							<div className="grid gap-2">
-								<div className="flex items-center gap-2 font-medium leading-none">
-									<Skeleton className="w-20 h-[10px] place-self-center rounded-xl" />
-								</div>
-							</div>
-						</div>
-					</CardFooter>
-				</Card>
-			</>
-		);
-	}
-	const ArrayChartData = Array(chartData)[0];
+  if (!chartData) {
+    return (
+      <>
+        <Card>
+          <CardHeader>
+            <CardTitle>Last 7 days</CardTitle>
+            <CardDescription>
+              Showing total messages for the last 7 days
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Skeleton className="w-[250px] h-[150px] place-self-center rounded-xl" />
+          </CardContent>
+          <CardFooter>
+            <div className="flex w-full items-start gap-2 text-sm">
+              <div className="grid gap-2">
+                <div className="flex items-center gap-2 font-medium leading-none">
+                  <Skeleton className="w-20 h-[10px] place-self-center rounded-xl" />
+                </div>
+              </div>
+            </div>
+          </CardFooter>
+        </Card>
+      </>
+    );
+  }
 
-	const startingDate = new Date(Date.now() - 0 * 24 * 60 * 60 * 1000);
-	const startingDate_formatted = `${(startingDate.getUTCMonth() + 1).toString().padStart(2, "0")}-${startingDate.getUTCDate().toString().padStart(2, "0")}`;
+  try {
+    const ArrayChartData = Array(chartData)[0];
 
-	const previousDate = new Date(Date.now() - 1 * 24 * 60 * 60 * 1000);
-	const previousDate_formatted = `${(previousDate.getUTCMonth() + 1).toString().padStart(2, "0")}-${previousDate.getUTCDate().toString().padStart(2, "0")}`;
+    const startingDate = new Date(Date.now() - 0 * 24 * 60 * 60 * 1000);
+    const startingDate_formatted = `${(startingDate.getUTCMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${startingDate
+      .getUTCDate()
+      .toString()
+      .padStart(2, "0")}`;
 
-	const currentAmount_index = ArrayChartData.findIndex(
-		(item) => item.date === startingDate_formatted,
-	);
-	const currentAmount = ArrayChartData[currentAmount_index].amount;
+    const previousDate = new Date(Date.now() - 1 * 24 * 60 * 60 * 1000);
+    const previousDate_formatted = `${(previousDate.getUTCMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${previousDate
+      .getUTCDate()
+      .toString()
+      .padStart(2, "0")}`;
 
-	const previousAmount_index = ArrayChartData.findIndex(
-		(item) => item.date === previousDate_formatted,
-	);
-	const previousAmount = ArrayChartData[previousAmount_index].amount;
+    const currentAmount_index = ArrayChartData.findIndex(
+      (item) => item.date === startingDate_formatted
+    );
+    const currentAmount = ArrayChartData[currentAmount_index].amount;
 
-	const difference = currentAmount - previousAmount;
+    const previousAmount_index = ArrayChartData.findIndex(
+      (item) => item.date === previousDate_formatted
+    );
+    const previousAmount = ArrayChartData[previousAmount_index].amount;
 
-	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>Last 7 days</CardTitle>
-				<CardDescription>
-					Showing total messages for the last 7 days
-				</CardDescription>
-			</CardHeader>
-			<CardContent>
-				<ChartContainer config={chartConfig}>
-					<AreaChart
-						accessibilityLayer
-						data={chartData}
-						margin={{
-							left: 12,
-							right: 12,
-						}}
-					>
-						<CartesianGrid vertical={false} />
-						<XAxis
-							dataKey="date"
-							tickLine={true}
-							axisLine={true}
-							tickMargin={8}
-							tickFormatter={(value) => value.slice(0, 5)}
-						/>
-						<ChartTooltip
-							cursor={true}
-							content={<ChartTooltipContent indicator="dot" hideLabel />}
-						/>
-						<Area
-							dataKey="amount"
-							type="linear"
-							fill="var(--color-amount)"
-							fillOpacity={0.4}
-							stroke="var(--color-amount)"
-						/>
-					</AreaChart>
-				</ChartContainer>
-			</CardContent>
-			<CardFooter>
-				<div className="flex w-full items-start gap-2 text-sm">
-					<div className="grid gap-2">
-						<div className="flex items-center gap-2 font-medium leading-none">
-							{difference > 0 ? (
-								<div className="text-green-400">
-									+{difference} than yesterday{" "}
-									<TrendingUp className="inline h-4 w-4" />
-								</div>
-							) : difference !== 0 ? (
-								<div className="text-red-400">
-									{difference} than yesterday{" "}
-									<TrendingDown className="inline h-4 w-4" />
-								</div>
-							) : (
-								<div className="text-gray-400">
-									+{difference} than yesterday
-								</div>
-							)}
-						</div>
-					</div>
-				</div>
-			</CardFooter>
-		</Card>
-	);
+    const difference = currentAmount - previousAmount;
+
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>Last 7 days</CardTitle>
+          <CardDescription>
+            Showing total messages for the last 7 days
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <AreaChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="date"
+                tickLine={true}
+                axisLine={true}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 5)}
+              />
+              <ChartTooltip
+                cursor={true}
+                content={<ChartTooltipContent indicator="dot" hideLabel />}
+              />
+              <Area
+                dataKey="amount"
+                type="linear"
+                fill="var(--color-amount)"
+                fillOpacity={0.4}
+                stroke="var(--color-amount)"
+              />
+            </AreaChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter>
+          <div className="flex w-full items-start gap-2 text-sm">
+            <div className="grid gap-2">
+              <div className="flex items-center gap-2 font-medium leading-none">
+                {difference > 0 ? (
+                  <div className="text-green-400">
+                    +{difference} than yesterday{" "}
+                    <TrendingUp className="inline h-4 w-4" />
+                  </div>
+                ) : difference !== 0 ? (
+                  <div className="text-red-400">
+                    {difference} than yesterday{" "}
+                    <TrendingDown className="inline h-4 w-4" />
+                  </div>
+                ) : (
+                  <div className="text-gray-400">
+                    +{difference} than yesterday
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </CardFooter>
+      </Card>
+    );
+  } catch (err) {
+    return (
+      <>
+        <Card>
+          <CardHeader>
+            <CardTitle>Last 7 days</CardTitle>
+            <CardDescription>
+              Showing total messages for the last 7 days
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="text-center">Not enough data</div>
+          </CardContent>
+        </Card>
+      </>
+    );
+  }
 }
