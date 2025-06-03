952395dea912fba6fea45317b2b81c53b4bbf42a
skellgreco <tixisb@gmail.com>
2025-06-03 20:29:02 +0300

    Added gradients to message activity charts

--- cially-web/app/dashboard/messages/_components/_message-charts/last_24hrs.tsx
+++ cially-web/app/dashboard/messages/_components/_message-charts/last_24hrs.tsx
@@ -112,13 +112,27 @@ export default function Last24h({ chartData }) {
                 cursor={true}
                 content={<ChartTooltipContent indicator="dot" hideLabel />}
               />
-
+              <defs>
+                <linearGradient id="fillGradient" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="5%"
+                    stopColor="var(--color-hour)"
+                    stopOpacity={0.8}
+                  />
+                  <stop
+                    offset="95%"
+                    stopColor="var(--color-hour)"
+                    stopOpacity={0.1}
+                  />
+                </linearGradient>
+              </defs>
               <Area
                 dataKey="amount"
-                type="linear"
-                fill="var(--color-hour)"
+                type="monotone"
+                fill="url(#fillGradient)"
                 fillOpacity={0.4}
                 stroke="var(--color-hour)"
+                stackId="a"
               />
             </AreaChart>
           </ChartContainer>
--- cially-web/app/dashboard/messages/_components/_message-charts/last_7d.tsx
+++ cially-web/app/dashboard/messages/_components/_message-charts/last_7d.tsx
@@ -98,18 +98,40 @@ export default function Last7d({ chartData }) {
                 axisLine={true}
                 tickMargin={8}
                 tickFormatter={(value) => value.slice(0, 5)}
+                interval={0}
+                tick={{
+                  angle: -30,
+                  fontSize: 10,
+                  dx: -5,
+                  dy: 5,
+                }}
               />
               <ChartTooltip
                 cursor={true}
                 content={<ChartTooltipContent indicator="dot" hideLabel />}
               />
-              <Area
-                dataKey="amount"
-                type="linear"
-                fill="var(--color-amount)"
-                fillOpacity={0.4}
-                stroke="var(--color-amount)"
-              />
+              <defs>
+                <linearGradient id="fillGradient" x1="0" y1="0" x2="0" y2="1">
+                  <stop
+                    offset="5%"
+                    stopColor="var(--color-amount)"
+                    stopOpacity={0.8}
+                  />
+                  <stop
+                    offset="95%"
+                    stopColor="var(--color-amount)"
+                    stopOpacity={0.1}
+                  />
+                </linearGradient>
+                </defs>
+                <Area
+                  dataKey="amount"
+                  type="monotone"
+                  fill="url(#fillGradient)"
+                  fillOpacity={0.4}
+                  stroke="var(--color-amount)"
+                  stackId="b"
+                />
             </AreaChart>
           </ChartContainer>
         </CardContent>
