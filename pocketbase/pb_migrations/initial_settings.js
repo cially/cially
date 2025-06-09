592ec8b89a94d9529a4412d7bf1cabcf3e632528
Keksi <me@keksi.dev>
2025-06-09 12:08:10 +0200

    feat: update initial settings migration to include application metadata and superuser creation

--- pocketbase/pb_migrations/initial_settings.js
+++ pocketbase/pb_migrations/initial_settings.js
@@ -1,13 +1,23 @@
 /// <reference path="../pb_data/types.d.ts" />
 migrate((app) => {
-    let superusers = app.findCollectionByNameOrId("_superusers")
-  
-      let record = new Record(superusers)
-  
-      record.set("email", $os.getenv("SUPERUSER_EMAIL") || "admin@cially.org")
-      record.set("password", $os.getenv("SUPERUSER_PASSWORD") || "admin")
-  
-      app.save(record)
+  // initial settings
+  let settings = app.settings()
+
+  settings.meta.appName = "Cially"
+  settings.meta.hideControls = true
+  settings.logs.minLevel = 4
+
+  app.save(settings)
+
+  // create superuser
+  let superusers = app.findCollectionByNameOrId("_superusers")
+
+  let record = new Record(superusers)
+
+  record.set("email", $os.getenv("SUPERUSER_EMAIL") || "admin@cially.org")
+  record.set("password", $os.getenv("SUPERUSER_PASSWORD") || "admin")
+
+  app.save(record)
   }, (app) => {
     // add down queries...
   })
