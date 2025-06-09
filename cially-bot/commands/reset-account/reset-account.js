8c036041c0ec97455612d231412209b710ad151b
skellgreco <tixisb@gmail.com>
2025-06-09 21:41:57 +0300

    Added /reset-account discord command so server admins can reset their code in case they forgot their password. Owner-Only command

--- cially-bot/commands/reset-account/reset-account.js
+++ cially-bot/commands/reset-account/reset-account.js
@@ -1,6 +1,12 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, MessageFlags } = require("discord.js");
 const { debug } = require("../../terminal/debug");
 const { error } = require("../../terminal/error");
+
+const PocketBase = require("pocketbase/cjs");
+const url = process.env.POCKETBASE_URL;
+const pb = new PocketBase(url);
+
+const guild_collection_name = process.env.GUILD_COLLECTION;
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,15 +15,47 @@ module.exports = {
 			"Use this command to delete your Cially Admin Account in order to create a new one.",
 		),
 	async execute(interaction) {
-		debug({ text: `Admin Account reset request sent` });
 		if (
 			interaction.guild &&
 			interaction.user.id === interaction.guild.ownerId
 		) {
-			console.log("This user is the server owner!");
+			try {
+				debug({ text: `Admin Account reset request sent` });
+
+				await pb
+					.collection("_superusers")
+					.authWithPassword(
+						process.env.POCKETBASE_ADMIN_EMAIL,
+						process.env.POCKETBASE_ADMIN_PASSWORD,
+					);
+				const record = await pb
+					.collection("users")
+					.getFirstListItem("admin=true", {});
+
+				await pb.collection("users").delete(record.id);
+				debug({
+					text: `Deleted Admin Account with id ${record.id} succesfully`,
+				});
+				await interaction.reply({
+					content: "Admin Account Deleted succesfully",
+					flags: MessageFlags.Ephemeral,
+				});
+			} catch (err) {
+				error({
+					text: `Something went wrong after trying to delete admin account`,
+				});
+
+				console.log(err);
+				await interaction.reply({
+					content: "There was an error when trying to delete admin account. Please check your logs",
+					flags: MessageFlags.Ephemeral,
+				});
+			}
 		} else {
-			console.log("This user is NOT the server owner.");
+			await interaction.reply({
+				content: "Only the Server Owner is allowed to use this command",
+				flags: MessageFlags.Ephemeral,
+			});
 		}
-		await interaction.reply("xxx");
 	},
 };
