59afca500de0843c4e0f01d7449b6bc5cc902226
Keksi <me@keksi.dev>
2025-06-11 09:00:56 +0200

    fix: type issue

--- cially-web/app/layout.tsx
+++ cially-web/app/layout.tsx
@@ -16,7 +16,7 @@ export const viewport: Viewport = {
   userScalable: false,
 };
 
-export default async function RootLayout({ children }: RootLayoutProps) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const cookieStore = await cookies();
   const theme = cookieStore.get("theme") || { value: "blue" };
   const themeClass = {
