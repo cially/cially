3ea2b52b34568d67425214f53324fe572f479663
skellgreco <tixisb@gmail.com>
2025-06-13 14:19:43 +0300

    Made the icon for active channels chart toggle to change according to which chart is currently being shown

--- apps/web/app/dashboard/server/activity/_components/active_channels.tsx
+++ apps/web/app/dashboard/server/activity/_components/active_channels.tsx
@@ -9,7 +9,7 @@ import {
   YAxis,
 } from "recharts";
 
-import { ArrowDown01 } from "lucide-react";
+import { ArrowDown01, ChartPie } from "lucide-react";
 
 import {
   Card,
@@ -80,80 +80,81 @@ export default function ActiveChannels({
               className="place-self-end rounded-full bg-white/0 hover:bg-white/10 transition-all p-0.5"
               onClick={() => numericToggle()}
             >
-              <ArrowDown01 />
+              {useNumeric === false ? <ArrowDown01 /> : <ChartPie />}
             </CardTitle>
           </div>
           <CardDescription>Last 4 weeks</CardDescription>
         </CardHeader>
         <CardContent className="pb-0">
-
-          { (useNumeric === false) ? 
-
-<ChartContainer config={chartConfig} className="aspect-square w-full">
-            <RadarChart data={chartData}>
-              <ChartTooltip cursor={true} content={<ChartTooltipContent />} />
-              <PolarAngleAxis dataKey="channel" />
-              <PolarGrid />
-              <Radar
-                dataKey="amount"
-                fill="var(--color-channel)"
-                fillOpacity={0.6}
-                dot={{
-                  r: 4,
-                  fillOpacity: 1,
+          {useNumeric === false ? (
+            <ChartContainer
+              config={chartConfig}
+              className="aspect-square w-full"
+            >
+              <RadarChart data={chartData}>
+                <ChartTooltip cursor={true} content={<ChartTooltipContent />} />
+                <PolarAngleAxis dataKey="channel" />
+                <PolarGrid />
+                <Radar
+                  dataKey="amount"
+                  fill="var(--color-channel)"
+                  fillOpacity={0.6}
+                  dot={{
+                    r: 4,
+                    fillOpacity: 1,
+                  }}
+                />
+              </RadarChart>
+            </ChartContainer>
+          ) : (
+            <ChartContainer config={chartConfig}>
+              <BarChart
+                accessibilityLayer
+                data={chartData}
+                layout="vertical"
+                margin={{
+                  right: 16,
                 }}
-              />
-            </RadarChart>
-          </ChartContainer>
-          : 
-          <ChartContainer config={chartConfig}>
-                      <BarChart
-                        accessibilityLayer
-                        data={chartData}
-                        layout="vertical"
-                        margin={{
-                          right: 16,
-                        }}
-                      >
-                        <CartesianGrid horizontal={false} />
-                        <YAxis
-                          dataKey="channel"
-                          type="category"
-                          tickLine={false}
-                          tickMargin={10}
-                          axisLine={false}
-                          tickFormatter={(value) => value.slice(0, 3)}
-                          hide
-                        />
-                        <XAxis dataKey="amount" type="number" hide />
-                        <ChartTooltip
-                          cursor={false}
-                          content={<ChartTooltipContent indicator="line" />}
-                        />
-                        <Bar
-                          dataKey="amount"
-                          layout="vertical"
-                          fill="#0370ff"
-                          radius={4}
-                        >
-                          <LabelList
-                            dataKey="channel"
-                            position="insideRight"
-                            offset={8}
-                            className="fill-white"
-                            fontSize={12}
-                          />
-                          <LabelList
-                            dataKey="amount"
-                            position="right"
-                            offset={8}
-                            className="fill-foreground"
-                            fontSize={12}
-                          />
-                        </Bar>
-                      </BarChart>
-                    </ChartContainer>
-                    }
+              >
+                <CartesianGrid horizontal={false} />
+                <YAxis
+                  dataKey="channel"
+                  type="category"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                  hide
+                />
+                <XAxis dataKey="amount" type="number" hide />
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent indicator="line" />}
+                />
+                <Bar
+                  dataKey="amount"
+                  layout="vertical"
+                  fill="#0370ff"
+                  radius={4}
+                >
+                  <LabelList
+                    dataKey="channel"
+                    position="insideRight"
+                    offset={8}
+                    className="fill-white"
+                    fontSize={12}
+                  />
+                  <LabelList
+                    dataKey="amount"
+                    position="right"
+                    offset={8}
+                    className="fill-foreground"
+                    fontSize={12}
+                  />
+                </Bar>
+              </BarChart>
+            </ChartContainer>
+          )}
         </CardContent>
         <CardFooter className="flex items-center justify-center gap-2 text-sm">
           <div className="font-medium leading-none">
@@ -166,12 +167,12 @@ export default function ActiveChannels({
   } catch (_err) {
     return (
       <Card className="h-full w-full">
-      <CardHeader>
-      <CardTitle>Most Active Channels</CardTitle>
-      <CardDescription>Last 4 weeks</CardDescription>
-      </CardHeader>
+        <CardHeader>
+          <CardTitle>Most Active Channels</CardTitle>
+          <CardDescription>Last 4 weeks</CardDescription>
+        </CardHeader>
         <CardContent className="pb-0">
-        <div className="text-center">Not enough data</div>
+          <div className="text-center">Not enough data</div>
         </CardContent>
       </Card>
     );
