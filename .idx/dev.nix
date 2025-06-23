76d16d1f719f0c09a3ec832308734f25655cd23d
marlonhotz07 <marlonhotz07@gmail.com>
2025-06-23 11:21:48 +0000

    chore: add docker for pocketbase

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -15,10 +15,11 @@
     pkgs.docker-buildx
   ];
 
-  # Sets environment variables in the workspace
-  env = {
+  services.docker.enable = true;
 
-  };
+  # Sets environment variables in the workspace
+  env = {};
+  
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
