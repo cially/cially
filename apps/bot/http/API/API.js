0844c9b4b360d860bb9169b923c522679aa657f6
skellgreco <tixisb@gmail.com>
2025-07-14 15:47:56 +0300

    Added ratelimits on express API routes

--- apps/bot/http/API/API.js
+++ apps/bot/http/API/API.js
@@ -19,6 +19,18 @@ const { serverScrape } = require("./functions/serverScrape");
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+const rateLimit = require('express-rate-limit');
+
+const globalRateLimiter = rateLimit({
+  windowMs: 1 * 1000, // 1 second
+  max: 10, // limit each IP to 5 requests per windowMs
+  message: {
+    error: 'Too many API requests, please try again later.'
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 // API Routes
 async function API(client) {
   // Main Listener
@@ -31,44 +43,44 @@ async function API(client) {
     syncGuild(req, res, client);
   });
 
-  app.get("/fetchGuilds", (req, res) => {
+  app.get("/fetchGuilds", globalRateLimiter, (req, res) => {
     fetchGuilds(req, res, client);
   });
 
-  app.get("/serverScrape/:guildID", (req, res) => {
+  app.get("/serverScrape/:guildID", globalRateLimiter, (req, res) => {
     serverScrape(req, res, client);
   });
 
   // POST Routes
-  app.post("/messageCreate/:guildID", (req, res) => {
+  app.post("/messageCreate/:guildID", globalRateLimiter, (req, res) => {
     messageCreate(req, res, client);
   });
 
-  app.post("/inviteCreate/:guildID", (req, res) => {
+  app.post("/inviteCreate/:guildID", globalRateLimiter, (req, res) => {
     inviteCreate(req, res, client);
   });
 
-  app.post("/guildMemberAdd/:guildID", (req, res) => {
+  app.post("/guildMemberAdd/:guildID", globalRateLimiter, (req, res) => {
     guildMemberAdd(req, res, client);
   });
 
-  app.post("/guildMemberRemove/:guildID", (req, res) => {
+  app.post("/guildMemberRemove/:guildID", globalRateLimiter, (req, res) => {
     guildMemberRemove(req, res, client);
   });
 
-  app.post("/fetchID/:guildID", (req, res) => {
+  app.post("/fetchID/:guildID", globalRateLimiter, (req, res) => {
     fetchID(req, res, client);
   });
 
-  app.post("/messageDelete/:guildID", (req, res) => {
+  app.post("/messageDelete/:guildID", globalRateLimiter, (req, res) => {
     messageDelete(req, res, client);
   });
 
-  app.post("/messageEdit/:guildID", (req, res) => {
+  app.post("/messageEdit/:guildID", globalRateLimiter, (req, res) => {
     messageEdit(req, res, client);
   });
 
-  app.post("/fetchUserData/:guildID", (req, res) => {
+  app.post("/fetchUserData/:guildID", globalRateLimiter, (req, res) => {
     fetchUserData(req, res, client);
   });
 }
