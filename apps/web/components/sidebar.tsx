c31a689a4adae42d1a4c7735ddc3bd5badd874d8
skellgreco <tixisb@gmail.com>
2026-03-01 11:21:49 +0200

    chore: Removed border from active sidebar element

--- apps/web/components/sidebar.tsx
+++ apps/web/components/sidebar.tsx
@@ -125,11 +125,10 @@ function ClientComponent({ isGuild }: { isGuild: boolean }) {
               <SidebarMenu>
                 {items.map((item) => (
                   <SidebarMenuItem
-                    className={`rounded-sm from-white/0 to-white/10 transition-all hover:bg-gradient-to-r ${
-                      isActive(item.path)
-                        ? "border-gray-400 border-l-2 bg-gradient-to-r from-white/2 to-white/10"
-                        : ""
-                    }`}
+                    className={`rounded-sm from-white/0 to-white/10 transition-all hover:bg-linear-to-r ${isActive(item.path)
+                      ? "border-gray-400 border-0 bg-linear-to-r from-white/2 to-white/10"
+                      : ""
+                      }`}
                     key={item.title}
                   >
                     <SidebarMenuButton asChild>
@@ -155,11 +154,10 @@ function ClientComponent({ isGuild }: { isGuild: boolean }) {
           <SidebarMenu>
             {cially_items.map((item) => (
               <SidebarMenuItem
-                className={`rounded-sm from-white/0 to-white/10 transition-all hover:bg-gradient-to-r ${
-                  isActive(item.path)
-                    ? "border-gray-400 border-l-2 bg-gradient-to-r from-white/2 to-white/10"
-                    : ""
-                }`}
+                className={`rounded-sm from-white/0 to-white/10 transition-all hover:bg-gradient-to-r ${isActive(item.path)
+                  ? "border-gray-400 border-l-2 bg-gradient-to-r from-white/2 to-white/10"
+                  : ""
+                  }`}
                 key={item.title}
               >
                 <SidebarMenuButton asChild>
