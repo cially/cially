ddd25c00848f7771a877e91a6083f9a9c535c952
skellgreco <tixisb@gmail.com>
2026-05-28 11:21:08 +0300

    bump: Bumped BETA Version to 10 for release

--- apps/web/components/sidebar.tsx
+++ apps/web/components/sidebar.tsx
@@ -174,7 +174,7 @@ function ClientComponent({ isGuild }: { isGuild: boolean }) {
       <SidebarFooter className="place-items-center">
         <a href="https://github.com/skellgreco/cially">
           <Badge className="rounded-full text-white/70 bg-white/10 backdrop-blur-lg" variant="secondary">
-            Version: 2.0 (BETA 9)
+            Version: 2.0 (BETA 10)
           </Badge>
         </a>
       </SidebarFooter>
