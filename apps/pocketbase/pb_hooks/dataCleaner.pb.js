db038b4f63acbe6b32c1ec57d94221f29f727889
skellgreco <tixisb@gmail.com>
2026-07-10 00:21:25 +0300

    refactor: add job completion notification

--- apps/pocketbase/pb_hooks/dataCleaner.pb.js
+++ apps/pocketbase/pb_hooks/dataCleaner.pb.js
@@ -3,26 +3,28 @@ cronAdd("dataCleaner", "0 0 * * *", () => {
 
     try {
         console.log('[dataCleaner CJ] Starting data cleaning process... ')
-        
+
         function clearOldData(collection) {
             const cutoffDate = new Date()
             cutoffDate.setDate(cutoffDate.getDate() - 28)
             const cutoffStr = cutoffDate.toISOString().replace('T', ' ').substring(0, 19)
-            
+
             console.log(`[dataCleaner CJ] Deleting ${collection} records older than: ${cutoffStr}`)
-            
+
             $app.db()
                 .newQuery(`DELETE FROM ${collection} WHERE created < {:cutoff}`)
                 .bind({ cutoff: cutoffStr })
                 .execute()
         }
-        
+
         clearOldData('channel_stats')
         clearOldData('hourly_stats')
         clearOldData('member_joins')
         clearOldData('member_leaves')
-        
-} catch (err) {
-    console.log(err)
-}
+
+        console.log('====== Job Finished ======')
+
+    } catch (err) {
+        console.log(err)
+    }
 })
